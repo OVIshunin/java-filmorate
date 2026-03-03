447425400db3dc0960c35bc76a2b9c8f3fd2a02f
IshuninOV <spart2007@yandex.ru>
2026-03-03 18:15:12 +0300

    Sprint 11, v1.1 fix: code-style

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -5,13 +5,23 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserStorage {
+
     User create(User user);
+
     User update(User user);
+
     List<User> findAll();
+
     Optional<User> findById(Integer id);
+
     boolean delete(Integer id);
+
     void addFriend(Integer userId, Integer friendId);
+
     void removeFriend(Integer userId, Integer friendId);
+
     List<User> getUserFriends(Integer userId);
+
     List<User> getCommonFriends(Integer userId, Integer otherId);
+
 }
